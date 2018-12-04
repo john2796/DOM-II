441f4d99c72efef8a8dd4bc45695012c2b02baef
john2796 <benedictmiranda27@yahoo.com>
2018-12-04 12:43:49 -0800

    added keydown

--- js/index.js
+++ js/index.js
@@ -44,3 +44,29 @@ function update() {
   x.innerHTML = `Browser inner window width: ${window.innerWidth}
                  , height: ${window.innerHeight}.`
 }
+
+
+// Mouse over 
+const logoHeading = document.querySelector('.logo-heading');
+logoHeading.addEventListener('mouseover', function (event) {
+  console.log(event);
+  logoHeading.textContent = 'Wild Bus !';
+})
+
+
+
+// Keydown 
+const quotes = [["Imitation is suicide.", "-Ralph Waldo Emerson"], ["Flatter yourself critically.", "-Willis Goth Regier"], ["Don’t look for society to give you permission to be yourself.", "-Steve Maraboli"], ["If things go wrong, don’t go with them.", "-Roger Babson"], ["Wanting to be someone else is a waste of who you are.", "-Kurt Cobain"], ["Do what you can, with what you have, where you are.", "-Theodore Roosevelt"], ["If you cannot be a poet, be the poem.", "-David Carradine"], ["Be there for others, but never leave yourself behind.", "-Dodinsky"]];
+
+function newQuote() {
+  const randomNumber = Math.floor(Math.random() * (quotes.length));
+  return quotes[randomNumber];
+}
+
+document.addEventListener('keydown', (event) => {
+  console.log(event)
+  const keyName = newQuote();
+  alert('keydown event\n\n' + 'key: ' + keyName);
+
+});
+
