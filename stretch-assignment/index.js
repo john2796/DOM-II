b966631e7d3daf09bff243682742a101be09b7b2
john2796 <benedictmiranda27@yahoo.com>
2018-12-05 09:04:26 -0800

    finished stretch assignment

---
+++ stretch-assignment/index.js
@@ -0,0 +1,40 @@
+const block = document.querySelectorAll(".block");
+
+let lastOrder = 0;
+let xIndex = {
+  "block--red": 0,
+  "block--blue": 0,
+  "block--green": 0,
+  "block--pink": 0,
+  "block--gray": 0
+}
+let interval;
+
+for (let i = 0; i <= block.length; i++) {
+  block[i].addEventListener('click', function () {
+    lastOrder -= 1;
+    block[i].style.order = `${lastOrder}`;
+  })
+  block[i].addEventListener('mousedown', function () {
+    interval = window.setInterval(function () {
+      //limit on the distance it can travel
+      if (xIndex[block[i].classList[1]] > 1000) {
+        console.log('past 1000')
+        xIndex[block[i].classList[1]] = 1000;
+      }
+      TweenMax.to(`.${block[i].classList[1]}`, 0.5, {
+        x: xIndex[block[i].classList[1]]++,
+      })
+    }, 10)
+    interval;
+    console.log("holding it down");
+  })
+  window.addEventListener('mouseup', function () {
+    window.clearInterval(interval);
+    //  When the mouse button is released, it should travel back to its original position.
+    TweenMax.to(`.${block[i].classList[1]}`, 0.5, {
+      x: 0,
+    })
+    console.log("released");
+  })
+}
